bee346638a78e2d2f503d4f0254036ac94aa9fae
Thomas LEGRAND <thomas.legrand@alumni.univ-avignon.fr>
2021-06-12 18:58:38 +0200

    Finalisation et push final

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,5 @@ return [
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
